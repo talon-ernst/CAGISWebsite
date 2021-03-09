150f70f888f4a20a02316648e054757a34d00329
Taylor M <60191576+tmcvittie1153@users.noreply.github.com>
2021-03-09 12:37:35 -0500

    Update CAGISKidsContext.cs

--- CAGISWebsite/Models/CAGISKidsContext.cs
+++ CAGISWebsite/Models/CAGISKidsContext.cs
@@ -38,7 +38,7 @@ namespace CAGISWebsite.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-4MMOC5A\\SQLEXPRESS;Database=CAGISKids;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CAGISKids;Trusted_Connection=True;");
             }
         }
 
